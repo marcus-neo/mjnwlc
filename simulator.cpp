ee567728f3f169164eab9323417b45c086cd57ac
Wei Loon <47574881+cwlroda@users.noreply.github.com>
2019-10-30 14:52:45 +0000

    moar memory

--- simulator.cpp
+++ simulator.cpp
@@ -6,10 +6,8 @@
 #include "Instructions.hpp"
 #include "decodeinstruction.hpp"
 
-
-void simulator::execute(){
-    int ProgCount = 124124912094; //PUT THIS TO A GLOBAL VARIABLE IN THE FUTURE
-    RAM r;
+void simulator::execute(RAM& r){
+    int ProgCount = r.ADDR_INSTR_OFFSET; //PUT THIS TO A GLOBAL VARIABLE IN THE FUTURE
     instructions ins;
     unsigned char address;
     unsigned long binaryinstruction;
@@ -17,6 +15,7 @@ void simulator::execute(){
     unsigned short decoded2;
     unsigned long decoded3;
     int instructiontype;
+
     while(ProgCount != 0){
         binaryinstruction = r.pullfromMemory(ProgCount);
         instructiontype = decodeinstruction(binaryinstruction, decoded1, decoded2, decoded3);
