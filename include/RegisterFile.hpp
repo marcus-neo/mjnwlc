fd907b68833a4f2a0b4ac120016b28b918de8754
marcus418 <56596363+marcus418@users.noreply.github.com>
2019-10-31 12:44:07 +0000

    nth

--- include/RegisterFile.hpp
+++ include/RegisterFile.hpp
@@ -6,8 +6,8 @@
 class RegisterFile{
     private: 
     long $zero;
-    long $at = 0x7fffffff;
-    long $v0 = 0x7fffffff, $v1 = 0x7fffffff;
+    long $at = 0x80000000;
+    long $v0 = 0x80000000, $v1 = 0x80000000;
     long $a0 = 0x10000000, $a1, $a2, $a3;
     long $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7;
     long $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7;
