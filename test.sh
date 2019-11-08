b4df8e6f2ebecfee7360d26d76ec4b5674e21407
marcus418 <56596363+marcus418@users.noreply.github.com>
2019-11-08 12:31:49 +0000

    asd

--- test.sh
+++ test.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 
 for i in bin/testbench/*.bin ; do
-if [ -f $i ]; then
-	cp $i bin/test/temp.bin; 
+	if [ -f "$i" ] ; then
+		cp $i bin/test/temp.bin; 
 
-	filename=$(basename $i .bin);
-	if [ -f "bin/testanswer/$filename.txt" ] ; then
-		dos2unix bin/testanswer/$filename.txt;
-		output=$(<bin/testanswer/$filename.txt);
-		echo $filename;
-		echo $output;
-	else
-		echo "bin/testanswer/$filename.txt not found!"
-		rm bin/test/temp.bin;
-		exit 0
-	fi
-
-	if [ -z "$1" ] ;	then
-		./prog
-	else
-		if [ -f "$1" ] ; then
-			./$1
+		filename=$(basename $i .bin);
+		if [ -f "bin/testanswer/$filename.txt" ] ; then
+			dos2unix bin/testanswer/$filename.txt;
+			output=$(<bin/testanswer/$filename.txt);
+			echo $filename;
+			echo $output;
 		else
-			echo "The file $1 does not exist!"
+			echo "bin/testanswer/$filename.txt not found!"
+			rm bin/test/temp.bin;
 			exit 0
 		fi
-	fi
 
-	RETCODE=$?
-	echo $RETCODE
+		if [ -z "$1" ] ;	then
+			./prog
+		else
+			if [ -f "$1" ] ; then
+				./$1
+			else
+				echo "The file $1 does not exist!"
+				exit 0
+			fi
+		fi
 
-	if [[ "$RETCODE" == "$output" ]] ; then
+		RETCODE=$?
+		echo $RETCODE
 
-		echo "Success";
+		if [[ "$RETCODE" == "$output" ]] ; then
+
+			echo "Success";
+		else
+			echo "Failure";
+		fi
+
+		rm bin/test/temp.bin;
+
+		echo $filename;
 	else
-		echo "Failure";
-
+		echo "bin/testbench contains no binary files!"
+		exit 0;
 	fi
-
-	rm bin/test/temp.bin;
-
-	echo $filename;
-else
-	echo "bin/testbench contains no binary files!"
-	exit 0;
-fi
 done
 
