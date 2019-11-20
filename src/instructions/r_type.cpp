f183d34b3472c45fbee4ec2aea79e3433eb1364f
Wei Loon <47574881+cwlroda@users.noreply.github.com>
2019-11-20 02:05:31 +0000

    Updates

--- src/instructions/r_type.cpp
+++ src/instructions/r_type.cpp
@@ -82,8 +82,8 @@ void jalr(unsigned short& rd, unsigned short rs){
 
     try{
         if((reg.readRegister(rs)%4 == 0 && reg.readRegister(rs) >= 0x10000000 && reg.readRegister(rs) < r.get_addr()) || reg.readRegister(rs) == 0){
-            delayins();
             reg.writeRegister(rd, PC.ProgCount+4);
+            delayins();
             PC.ProgCount = reg.readRegister(rs);
         }
 
